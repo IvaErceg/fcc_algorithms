0a66aedcc866d5902a00c57853fb4ff99200ec30
Iva <erceg_iva@yahoo.co.uk>
2018-01-11 10:54:58 +0100

    implement factorialize

--- index.js
+++ index.js
@@ -3,20 +3,44 @@
  * @param {Number} celsius - temperature in celsius
  * @return {Number} - temperature in fahrenheit
  */
-
 function convertToF(celsius) {
-    return celsius * 9/5 + 32;
-  }
-  
-  convertToF(30);
+  return celsius * 9 / 5 + 32;
+}
 
 /**
  * Reverses a string
  * @param {String} str - input string
  * @return {String} - reversed string
  */
-  function reverseString(str) {
-    return str.split("").reverse().join("");  //arrays has .reverse() method
+function reverseString(str) {
+  return str
+    .split("")
+    .reverse()
+    .join(""); //arrays has .reverse() method
+}
+
+/**
+ * Calculates the factorial using recursion
+ * @param {Number} num - input number
+ * @return {Number} -factorial
+ */
+function factorializeRecursive(num) {
+  if (num == 0) {
+    return 1;
   }
-  
-  reverseString("hello");
+  return num * factorializeRecursive(num - 1);
+}
+
+//with the loop
+function factorialize(num) {
+  let result = 1;
+  for (let i = num; i > 1; i--) {
+    result *= i;
+  }
+  return result;
+}
+
+convertToF(30);
+reverseString("hello");
+factorializeRecursive(10);
+factorialize(5);
